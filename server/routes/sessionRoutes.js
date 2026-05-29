b5ae9a0a9a781a4c2d2f2bc2d8a6f67d57520ea0
tonydemox <antoniodemaso20@gmail.com>
2026-05-29 23:55:29 +0200

    fix controller names

--- server/routes/sessionRoutes.js
+++ server/routes/sessionRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createSession, getSessions } = require('../controllers/sessioncontroller');
+const { createSession, getSessions } = require('../controllers/sessionController');
 
 router.post('/', createSession);
 router.get('/', getSessions);
